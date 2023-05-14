c6dc58628ae615017d37787ee57e5a368d4c8d94
R4R3-M37 <iw4narte44@yandex.ru>
2023-05-14 07:01:59 +0300

    • 🛠️ test (premierformatter.test.ts): change Jest premier formatter test case logic

--- src/shared/lib/tests/premierFormatter.test.ts
+++ src/shared/lib/tests/premierFormatter.test.ts
@@ -1,26 +1,36 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import { premierFormatter } from '~/shared/lib/premierFormatter'
+import { premierFormatter } from '../premierFormatter'
 
 describe('Конвертирование из минут в часы', () => {
+	const mockObjectPremier = {
+		world: '2011-09-23T00:00:00.000Z',
+		russia: '2012-04-26T00:00:00.000Z'
+	}
+	const shouldBe = [
+		{
+			country: 'Мир',
+			date: '23 сентября 2011'
+		},
+		{
+			country: 'Россия',
+			date: '26 апреля 2012'
+		}
+	]
+
 	test('Нормальное поведение', () => {
-		expect(premierFormatter(120)).toBe('2 часа')
-		expect(premierFormatter(60)).toBe('1 час')
-		expect(premierFormatter(40)).toBe('40 минут')
-		expect(premierFormatter(21)).toBe('21 минуту')
-		expect(premierFormatter(0)).toBeUndefined()
+		expect(premierFormatter(mockObjectPremier)).toBe(shouldBe)
+		expect(premierFormatter(0)).toEqual([])
 	})
 
 	test('Отрицательное число', () => {
-		expect(premierFormatter(-1000)).toBeUndefined()
-		expect(premierFormatter(-1)).toBeUndefined()
+		expect(premierFormatter(-1000)).toEqual([])
+		expect(premierFormatter(-1)).toEqual([])
 	})
 
 	test('Не число', () => {
-		expect(premierFormatter('120')).toBe('2 часа')
-		expect(premierFormatter(true)).toBe('1 минуту')
-		expect(premierFormatter(false)).toBeUndefined()
-		expect(premierFormatter([])).toBeUndefined()
-		expect(premierFormatter({})).toBeUndefined()
+		expect(premierFormatter(false)).toEqual([])
+		expect(premierFormatter([])).toEqual([])
+		expect(premierFormatter({})).toEqual([])
 	})
 })
