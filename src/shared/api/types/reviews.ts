a3a6d00e46e985850a260a826bfa966d89e88ad9
R4R3-M37 <iw4narte44@yandex.ru>
2023-06-21 17:29:25 +0500

    Revert "• 🔧 fix (reviews.ts): update optional properties to required in ReviewsResponse and ReviewsResponseItem interfaces"
    This reverts commit e3b90cf0c3ceb30aeddb98ec6bdf8ebbdc52d233.

--- src/shared/api/types/reviews.ts
+++ src/shared/api/types/reviews.ts
@@ -1,19 +1,19 @@
-interface ReviewsResponseItem {
-	kinopoiskId: number
-	type: 'POSITIVE' | 'NEGATIVE' | 'NEUTRAL' | 'UNKNOWN'
-	date: string
-	positiveRating: number
-	negativeRating: number
-	author: string
+export interface Items {
+	kinopoiskId?: number
+	type?: string
+	date?: string
+	positiveRating?: number
+	negativeRating?: number
+	author?: string
 	title?: string
-	description: string
+	description?: string
 }
 
-export interface ReviewsResponse {
-	total: number
-	totalPages: number
-	totalPositiveReviews: number
-	totalNegativeReviews: number
-	totalNeutralReviews: number
-	items: ReviewsResponseItem[]
+export interface ReviewsRoot {
+	total?: number
+	totalPages?: number
+	totalPositiveReviews?: number
+	totalNegativeReviews?: number
+	totalNeutralReviews?: number
+	items?: Items[]
 }
