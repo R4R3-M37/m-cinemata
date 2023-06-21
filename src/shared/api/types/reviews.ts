cfa53aa4953d0f8e3c8bcaefe12f7bf3edaf38b4
R4R3-M37 <iw4narte44@yandex.ru>
2023-06-21 17:31:31 +0500

    • ♻️ refactor (reviews.ts): update optional properties to required in ReviewsRoot and ReviewsRootItem interfaces

--- src/shared/api/types/reviews.ts
+++ src/shared/api/types/reviews.ts
@@ -1,19 +1,19 @@
-export interface Items {
-	kinopoiskId?: number
-	type?: string
-	date?: string
-	positiveRating?: number
-	negativeRating?: number
-	author?: string
+interface ReviewsItem {
+	kinopoiskId: number
+	type: 'POSITIVE' | 'NEGATIVE' | 'NEUTRAL' | 'UNKNOWN'
+	date: string
+	positiveRating: number
+	negativeRating: number
+	author: string
 	title?: string
-	description?: string
+	description: string
 }
 
 export interface ReviewsRoot {
-	total?: number
-	totalPages?: number
-	totalPositiveReviews?: number
-	totalNegativeReviews?: number
-	totalNeutralReviews?: number
-	items?: Items[]
+	total: number
+	totalPages: number
+	totalPositiveReviews: number
+	totalNegativeReviews: number
+	totalNeutralReviews: number
+	items: ReviewsItem[]
 }
